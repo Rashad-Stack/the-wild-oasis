41d4ce4e8c5e7ae304fe0892a059d43131f9e1f4
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-08-02 22:46:37 +0600

    ui: header ui fixed

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,16 +1,23 @@
 import { styled } from "styled-components";
-import Logout from "../features/authentication/Logout";
+import UserAvatar from "../features/authentication/UserAvatar";
+import HeaderMenu from "./HeaderMenu";
 
 const HeaderStyles = styled.header`
   background-color: var(--color-gray-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 export default function Header() {
   return (
     <HeaderStyles>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </HeaderStyles>
   );
 }
